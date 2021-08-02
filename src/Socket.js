cd7315b8f62a20da5a70cfc6e80d7d46020a44ec
Jhoemar <jhoemar_pagao@yahoo.com>
2021-08-02 19:33:49 +0800

    updated connection

--- src/Socket.js
+++ src/Socket.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react"
 import io from "socket.io-client"
-const PUBLIC_URI = window.location.hostname + ":9000"
-window.socket = io(PUBLIC_URI, { transports: ["websocket"] })
+const PUBLIC_URI = "http://localhost" + ":9000"
+const sio = io(PUBLIC_URI, { transports: ["websocket"]})
+window.socket = sio
 function Socket({ children }) {
   const [elaStatus, setElaStatus] = useState("")
   const socket = window.socket
   const handleCheckStatus = () => {
     socket.emit("elastatus", (currentStatus) => {
+      console.log("status " + currentStatus)
       setElaStatus(currentStatus)
     })
   }
@@ -15,13 +17,18 @@ function Socket({ children }) {
     socket.on("connect", () => {
       console.log("connected")
       handleCheckStatus()
-      socket.emit("ela.system.SUBSCRIBE", "ela.system")
+      socket.emit("ela.system", {"id": "ela.system.SUBSCRIBE", "data": "ela.system"}, response => {
+        console.log( response)
+      })
+    })
+    socket.on("ela.broadcast.SYSTEM_STATUS_CHANGED", (data) => {
+      console.log("Response from ela.system " + data)
     })
     socket.on("disconnect", () => {
       console.log("disconnected")
     })
     socket.on("connect_error", (response) => {
-      console.log(response)
+      console.log("ERRR " + response)
     })
     //eslint-disable-next-line
   }, [socket])
