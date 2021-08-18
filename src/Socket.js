91184e29adddc406fd3425b64277f5e0167cf708
jhoe123 <jhoemar_pagao@yahoo.com>
2021-08-18 06:28:30 +0000

    changed the socket port from 9000 to 80

--- src/Socket.js
+++ src/Socket.js
@@ -2,6 +2,25 @@ import { useEffect, useState } from "react"
 import io from "socket.io-client"
 const PUBLIC_URI = window.location.hostname + ":9000"
 export const socket = io(PUBLIC_URI, { transports: ["websocket"] })
+
+socket.on("connect", () => {
+  console.log("connected")
+  // subscribe to ela system
+  socket.emit(
+    "ela.system",
+    { id: "ela.system.SUBSCRIBE" },
+    (response) => {
+      console.log(response)
+    }
+  )
+})
+socket.on("disconnect", () => {
+  console.log("disconnected")
+})
+socket.on("connect_error", (response) => {
+  console.log("ERRR " + response)
+})
+
 function Socket({ children }) {
   const [elaStatus, setElaStatus] = useState("")
   const handleCheckStatus = () => {
@@ -11,26 +30,15 @@ function Socket({ children }) {
   }
   useEffect(() => {
     if (!socket) return
-    socket.emit(
-      "ela.system",
-      { id: "ela.system.SUBSCRIBE", data: "ela.system" },
-      (response) => {
-        console.log(response)
+    handleCheckStatus()
+    socket.on("ela.broadcast.SYSTEM_STATUS_CHANGED", (data) => {
+      if (typeof(data) === 'string') {
+        data = JSON.parse(data)
       }
-    )
-    socket.on("connect", () => {
-      console.log("connected")
-      handleCheckStatus()
-      socket.on("ela.broadcast.SYSTEM_STATUS_CHANGED", (data) => {
-        console.log("Response from ela.system " + data)
-      })
+      console.log("Response from ela.system " + data)
+      setElaStatus(data.status)
     })
-    socket.on("disconnect", () => {
-      console.log("disconnected")
-    })
-    socket.on("connect_error", (response) => {
-      console.log("ERRR " + response)
-    })
+    
     //eslint-disable-next-line
   }, [socket])
   return children({ elaStatus, handleCheckStatus })
